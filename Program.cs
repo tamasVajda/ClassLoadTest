ebb741c4cb66324beb65b3ce1eaa5b3fbe911695
tamasVajda <41056520+tamasVajda@users.noreply.github.com>
2019-03-04 08:47:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using DebendencyContainer;
+using System;
+
+namespace assemblyloadtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (new MethodManager().GetResult())
+                {
+                    Console.WriteLine("Evrithing OK.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Huston we have a problem: " + e.Message);
+            }
+        }
+    }
+}
